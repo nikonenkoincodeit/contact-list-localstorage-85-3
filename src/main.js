5aa791523e7a848ac177f8b0a28cd7fb1b27d577
ptrodion <rdnpetrenko@gmail.com>
2023-07-02 09:09:48 +0100

    add 1 part

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
+import { uid } from "uid";
+import { formEL } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEL.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  // const formDate = Object.fromEntries(new FormData(e.target));
+
+  const { name, number, email } = e.target.elements;
+  const currentObject = createObject(name.value, number.value, email.value);
+}
+
+function createObject(name, number, email) {
+  return { createdAt: Date.now(), id: uid(), name, number, email };
+}
--- src/refs/index.js
+++ src/refs/index.js
@@ -1,1 +1,2 @@
-
+const formEL = document.querySelector(".js-form");
+export { formEL };
